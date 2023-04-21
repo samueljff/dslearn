b1feb6bdb1c3caef3571de6453ddaad3981097f6
samueljff <samueljosef3@gmail.com>
2023-04-21 15:28:39 -0300

    Repositories

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/RoleRepositoy.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/RoleRepositoy.java
@@ -1,11 +1,9 @@
 package com.devsuperior.dslearnbds.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dslearnbds.entities.Role;
 
-@Repository
 public interface RoleRepositoy extends JpaRepository<Role, Long> {
 
 }
