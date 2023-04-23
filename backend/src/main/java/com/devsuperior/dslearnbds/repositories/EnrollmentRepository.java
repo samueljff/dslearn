1062bcafb9d059272d705da72b7d06a4d604e68c
samueljff <samueljosef3@gmail.com>
2023-04-23 17:34:41 -0300

    Custom content for current user

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/EnrollmentRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/EnrollmentRepository.java
@@ -2,8 +2,8 @@ package com.devsuperior.dslearnbds.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.devsuperior.dslearnbds.entities.Notification;
+import com.devsuperior.dslearnbds.entities.Enrollment;
 
-public interface EnrollmentRepository extends JpaRepository<Notification, Long> {
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
 
 }
