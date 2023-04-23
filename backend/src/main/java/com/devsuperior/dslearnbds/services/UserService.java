21f0f6bf2ad4e793a8b3b27b4b68af78aaed30db
samueljff <samueljosef3@gmail.com>
2023-04-22 22:47:59 -0300

    Exception, validation, security

--- backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
@@ -2,11 +2,15 @@ package com.devsuperior.dslearnbds.services;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,32 +21,45 @@ import com.devsuperior.dslearnbds.services.exceptions.DatabaseException;
 import com.devsuperior.dslearnbds.services.exceptions.ResourceNotFoundException;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
+	private static Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
+	
 	@Autowired
-	private UserRepository userrepository;
+	private UserRepository userRepository;
 
 	@Transactional(readOnly = true)
 	public Page<UserDTO> findAllPaged(Pageable pageable) {
-		Page<User> list = userrepository.findAll(pageable);
+		Page<User> list = userRepository.findAll(pageable);
 
 		return list.map(x -> new UserDTO(x));
 	}
 
 	@Transactional(readOnly = true)
 	public UserDTO findById(Long id) {
-		Optional<User> obj = userrepository.findById(id);
+		Optional<User> obj = userRepository.findById(id);
 		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found!"));
 		return new UserDTO(entity);
 	}
 
 	public void delete(Long id) {
 		try {
-			userrepository.deleteById(id);
+			userRepository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException("Id not Found! " + id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException("Integrity Violation!" + id);
 		}
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(username);
+		if (user == null) {
+			logger.error("User not found! " + username);
+			throw new UsernameNotFoundException("Email not found!");
+		}
+		logger.info("User found" + username);
+		return user;
 	}
 }
